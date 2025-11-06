dd2b00d1ded0d47bca7844740d2c29c4fcf3d8a4
Caleb Myers <cmyers5108@gmail.com>
2025-11-05 23:22:19 -0500

    node versioning

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import apiRoutes from "./routes/api";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://spotime.vercel.app"],
     credentials: true,
   })
 );
